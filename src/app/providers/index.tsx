c46175e410cc6f7365f6cb786f7f08a3ff63e2eb
Xaletch <kiril.kolesnikov0609@mail.ru>
2024-10-08 19:06:24 +0300

    telegram provider

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,9 +1,10 @@
 import { FC, PropsWithChildren } from 'react'
+import { TelegramProvider } from './telegram'
 
 export const Providers: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <>
+    <TelegramProvider>
       {children}
-    </>
+    </TelegramProvider>
   )
 }
