03f301613c9493b536def1482ccbf4af865a3484
Xaletch <kiril.kolesnikov0609@mail.ru>
2024-10-28 20:15:14 +0300

    +

--- src/app/providers/telegram/index.tsx
+++ src/app/providers/telegram/index.tsx
@@ -1,5 +1,7 @@
 import { createContext, useContext, useEffect, useMemo, useState } from "react";
 import { IWebApp, ITelegramUser } from "./telegram.types";
+import { useLocation } from "@tanstack/react-router"
+
 
 export interface ITelegramContext {
   webApp?: IWebApp;
@@ -13,24 +15,46 @@ export const TelegramProvider = ({
   children: React.ReactNode;
 }) => {
   const [webApp, setWebApp] = useState<IWebApp | null>(null);
+  const location = useLocation();
 
+  const hiddenButtonPaths = [
+    "/login",
+    "/forgot",
+    "/forgot/new-pin",
+    "/wallet",
+    "/seed-phrase",
+    "/seed-phrase/pin",
+    "/registration-pin",
+    "/phone",
+    "/phone-code",
+    "/wallet-created"
+  ];
+  
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const app = (window as any).Telegram?.WebApp;
     if (app) {
       app.ready();
       setWebApp(app);
+      console.log("web app initialized:", app);
 
-      // app.BackButton.onClick = () => {
-      //   console.log("history:", window.history.length);
-      //   if (window.history.length > 1) {
-      //     window.history.back();
-      //   } else {
-      //     app.close();
-      //   }
-      // };
+      app.BackButton.onClick = () => {
+        console.log("click");
+        window.history.back();
+      };
     }
   }, []);
+
+  useEffect(() => {
+    if (webApp) {
+      const backButton = webApp.BackButton;
+      if (hiddenButtonPaths.includes(location.pathname)) {
+        backButton.hide();
+      } else {
+        backButton.show();
+      }
+    }
+  }, [webApp, location.pathname]);
 
   const value = useMemo(() => {
     return webApp
--- src/templates/layout/index.tsx
+++ src/templates/layout/index.tsx
@@ -1,11 +1,9 @@
-import { FC, PropsWithChildren, useEffect } from "react"
+import { FC, PropsWithChildren } from "react"
 import { Navbar } from "@/widgets/navbar"
 import { useLocation } from "@tanstack/react-router"
-import { useTelegram } from "@/app/providers/telegram";
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
   const location = useLocation().pathname;
-  const { webApp } = useTelegram();
 
   const isLocation = [
     "/forgot", "/login", "/forgot/new-pin", "/registration-pin", "/seed-phrase",
@@ -16,39 +14,6 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
     "/profile/two-factor", "/profile/two-factor/enable", "/profile/two-factor/change-code", "/profile/two-factor/change/",
     "/trade", "/trade/hide", "/trade/token/id", "/trade/action", "/trade/wallets"
   ].includes(location) || /\/wallet\/\d+/.test(location) || /\/wallet\/token\/\d+/.test(location) || /\/wallet\/receive\/\d+/.test(location);
-
-  const hiddenButton = [
-    "/login",
-    "/forgot",
-    "/forgot/new-pin",
-    "/wallet",
-    "/seed-phrase",
-    "/seed-phrase/pin",
-    "/registration-pin",
-    "/phone",
-    "/phone-code",
-    "/wallet-created"
-  ];
-
-  useEffect(() => {
-    if (webApp) {
-      const backButton = webApp.BackButton;
-      console.log("button initialized:", backButton);
-
-      if (hiddenButton.includes(location)) {
-        backButton.hide();
-      } else {
-        backButton.show();
-      }
-
-      backButton.onClick = () => {
-        console.log("click");
-        window.history.back();
-      };
-    } else {
-      console.log("webApp not");
-    }
-  }, [location]);
 
   return (
     <div className="bg-muted flex flex-col flex-1 relative">
