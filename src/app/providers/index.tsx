3d873c9b6f47e4c3bf748116f17002af28eba1e6
Xaletch <kiril.kolesnikov0609@mail.ru>
2024-10-28 19:00:27 +0300

    +

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,23 +1,15 @@
-import { FC, PropsWithChildren, useEffect, useState } from 'react'
+import { FC, PropsWithChildren } from 'react'
 import { TelegramProvider } from './telegram'
 import { ThemeProvider } from './theme/Theme.provider'
 import { ReactNotifications } from 'react-notifications-component'
-import { useLocation } from "@tanstack/react-router"
 import 'react-notifications-component/dist/theme.css';
 
 
 export const Providers: FC<PropsWithChildren> = ({ children }) => {
-  const location = useLocation();
-  const [currentPath, setCurrentPath] = useState<string>(location.pathname);
-
-  useEffect(() => {
-    setCurrentPath(location.pathname);
-  }, [location.pathname]);
-
   return (
     <>
       <ReactNotifications />
-      <TelegramProvider currentPath={currentPath}>
+      <TelegramProvider >
         <ThemeProvider defaultTheme="light" storageKey="ui-theme">
           {children}
         </ThemeProvider>
