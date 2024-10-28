8a6d854cff6b175f18e412cc3a152e4fea8247cb
Xaletch <kiril.kolesnikov0609@mail.ru>
2024-10-28 21:40:20 +0300

    +

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "@/routeTree.gen";
 import { useEffect } from "react";
 import { useTelegram } from "./providers/telegram";
-import { useTelegramBackButton } from "@/shared/hooks/useTelegramBackButton";
+// import { useTelegramBackButton } from "@/shared/hooks/useTelegramBackButton";
 
 const router = createRouter({ routeTree });
 declare module "@tanstack/react-router" {
@@ -17,8 +17,7 @@ function App() {
   useEffect(() => {
     webApp?.expand();
     if (webApp) {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      useTelegramBackButton(webApp);
+      // useTelegramBackButton(webApp);
     }
   }, [webApp]);
 
